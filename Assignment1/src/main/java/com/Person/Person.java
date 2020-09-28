d30f803ee5fe44f1853403e242f3d7d1e672abfd
FengPeng92 <peng.fen@northeastern.edu>
2020-09-28 16:07:50 -0400

    Assignment1

---
+++ Assignment1/src/main/java/com/Person/Person.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Person;
+
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author fengpeng
+ */
+public class Person {
+    private String name;
+    private String geoData;
+    private String birth;
+    private String telephone;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String medicalRecord;
+    private String healthPlan;
+    private String bankAccount;
+    private String license;
+    private String vehicleNum;
+    private String deviceNum;
+    private String linkedin;
+    private String ipAddress;
+    private String biometricIdentifiers;
+    private Icon img;
+    private String anyUniqueNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public void setMedicalRecord(String medicalRecord) {
+        this.medicalRecord = medicalRecord;
+    }
+
+    public String getHealthPlan() {
+        return healthPlan;
+    }
+
+    public void setHealthPlan(String healthPlan) {
+        this.healthPlan = healthPlan;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getVehicleNum() {
+        return vehicleNum;
+    }
+
+    public void setVehicleNum(String vehicleNum) {
+        this.vehicleNum = vehicleNum;
+    }
+
+    public String getDeviceNum() {
+        return deviceNum;
+    }
+
+    public void setDeviceNum(String deviceNum) {
+        this.deviceNum = deviceNum;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometricIdentifiers() {
+        return biometricIdentifiers;
+    }
+
+    public void setBiometricIdentifiers(String biometricIdentifiers) {
+        this.biometricIdentifiers = biometricIdentifiers;
+    }
+
+    public Icon getImg() {
+        return img;
+    }
+
+    public void setImg(Icon img) {
+        this.img = img;
+    }
+
+    public String getAnyUniqueNum() {
+        return anyUniqueNum;
+    }
+
+    public void setAnyUniqueNum(String anyUniqueNum) {
+        this.anyUniqueNum = anyUniqueNum;
+    }
+     
+    
+}
